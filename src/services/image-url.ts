aaf1a7af9f59ef90d49d29fd886164eb11cf5f6d
Glenn Seymon <glenn@superdudes.com.au>
2026-05-14 10:10:00 +1000

    Handle games with no image.

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,3 +1,5 @@
+import noIMage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 const getCroppedImageUrl = (url: string) => {
 	const target = 'media/';
 
@@ -5,7 +7,7 @@ const getCroppedImageUrl = (url: string) => {
 		const index = url.indexOf(target) + target.length;
 		return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 	} catch {
-		return '';
+		return noIMage;
 	}
 };
 
