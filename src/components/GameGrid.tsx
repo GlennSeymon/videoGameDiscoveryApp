0d63d7814fb5154ec169780c25b5c0809acfec0a
Glenn Seymon <glenn@superdudes.com.au>
2026-05-22 13:27:22 +1000

    Add hover transitions for game cards.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,22 @@
-import { Alert, CircularProgress, Grid } from '@mui/material';
+import { Alert, CircularProgress, Grid, styled } from '@mui/material';
 import React from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
+
+const StyledGrid = styled(Grid)(({ theme }) => ({
+	margin: theme.spacing(2),
+}));
+
+const GameCardGrid = styled(Grid)(({ theme }) => ({
+	'&:hover': {
+		transform: 'scale(1.05)',
+		'box-shadow': '0 0 20px rgba(0, 255, 255, 0.5)',
+	},
+	transition: 'all 0.5s ease',
+	borderRadius: theme.spacing(2),
+}));
 
 const GameGrid = () => {
 	const { data, error, isLoading, fetchNextPage, hasNextPage } = useGames();
@@ -23,7 +36,7 @@ const GameGrid = () => {
 				loader={<CircularProgress />}
 				style={{ overflow: 'hidden' }}
 			>
-				<Grid container spacing={2}>
+				<StyledGrid container spacing={2}>
 					{isLoading &&
 						skeletons.map((skeleton) => (
 							<Grid
@@ -37,16 +50,16 @@ const GameGrid = () => {
 					{data?.pages.map((page, index) => (
 						<React.Fragment key={index}>
 							{page.results.map((game) => (
-								<Grid
+								<GameCardGrid
 									key={game.id}
 									size={{ xs: 12, sm: 6, md: 4, lg: 3, xl: 2 }}
 								>
 									<GameCard game={game} />
-								</Grid>
+								</GameCardGrid>
 							))}
 						</React.Fragment>
 					))}
-				</Grid>
+				</StyledGrid>
 			</InfiniteScroll>
 		</>
 	);
